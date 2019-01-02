d160a1f3ae33cd2536b77e905c76fcfc86b591e1
zhebinhu <huzhebin2017@gmail.com>
2019-01-03 01:42:33 +0800

    Site updated: 2019-01-03 01:42:21

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,9 @@
                 var data = res.list[j].arr;
                 var liTmpl = "";
                 for (var i = 0, len = data.link.length; i < len; i++) {
-                    var minSrc = 'http://p1x2xthig.bkt.clouddn.com//min_photo/' + data.link[i];
-                    var src = 'http://p1x2xthig.bkt.clouddn.com//src_photo/' + data.link[i];
+                    var minSrc = '\n' +
+                        'https://huzb.oss-cn-beijing.aliyuncs.com/min_' + data.link[i];
+                    var src = 'https://huzb.oss-cn-beijing.aliyuncs.com/src_' + data.link[i];
                     var type = data.type[i];
                     var target = src/* + (type === 'video' ? '.mp4' : '.jpg')*/;
                     /*src += '.jpg';*/
